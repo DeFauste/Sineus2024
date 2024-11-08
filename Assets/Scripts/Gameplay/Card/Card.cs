764313037aa0cf6695106df3f8cee27300f13982
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-11-08 16:57:11 +0300

    fix: delete old
    удалил старые префабы и скрипты. Добавил базовую сцену. Логику заполнения карты.

---
+++ Assets/Scripts/Gameplay/Card/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Card
+{
+    [CreateAssetMenu(fileName = "NewRune", menuName = "Gameplay/Card", order = 1)]
+    public class Card : ScriptableObject
+    {
+        public string Name = "Rune";
+        public List<Elements> Elements;
+        public Sprite Sprite;
+        public string Description = "Rune description";
+    }
+}
---
+++ Assets/Scripts/Gameplay/Card/CardBinder.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Gameplay.Card
+{
+    public class CardBinder : MonoBehaviour
+    {
+        [SerializeField] private Card _card;
+        [SerializeField] private Image _image;
+        [SerializeField] private TextMeshProUGUI _name;
+        [SerializeField] private TextMeshProUGUI _description;
+        public void SetCard(Card card) => _card = card;
+
+        private void Start()
+        {
+            UpdateCardView();
+        }
+
+        public void UpdateCardView()
+        {
+            if(_card != null)
+            {
+                _name.text = _card.Name;
+                _image.sprite = _card.Sprite;
+                _description.text = _card.Description;
+            }
+        }
+    }
+}
