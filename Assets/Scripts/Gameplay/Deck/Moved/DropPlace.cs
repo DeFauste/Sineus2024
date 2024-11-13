1326acb605fcb6912785f556e0f0160c1fadac7b
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-11-13 16:28:25 +0300

    add: перетаскивания карт

---
+++ Assets/Scripts/Gameplay/Deck/Moved/DropPlace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Gameplay.Deck.Moved
+{
+    public class DropPlace : MonoBehaviour, IDropHandler
+    {
+        public void OnDrop(PointerEventData eventData)
+        {
+            HoverEffect hoverEffect = eventData.pointerDrag.GetComponent<HoverEffect>();
+            if (hoverEffect != null)
+            {
+                hoverEffect._defaultParent = transform;
+            }
+        }
+    }
+}
--- Assets/Scripts/Gameplay/Deck/Moved/HoverEffect.cs
+++ Assets/Scripts/Gameplay/Deck/Moved/HoverEffect.cs
@@ -2,25 +2,56 @@
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
-public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform targetImage;
     public Vector3 hoverOffset = new Vector3(0, 10, 0); 
     public float duration = 0.5f;
-
+    private Camera _camera;
+    Vector3 offset;
+    private bool _isDragging = false;
+    public Transform _defaultParent;
+    private CanvasGroup _canvasGroup;
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
     private void Start()
     {
         targetImage = GetComponent<RectTransform>();
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        targetImage.DOLocalMove(targetImage.localPosition + hoverOffset, duration)
-                   .SetEase(Ease.OutBack); 
+      // if(!_isDragging) targetImage.DOLocalMove(targetImage.localPosition + hoverOffset, duration)
+                 //  .SetEase(Ease.OutBack); 
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        targetImage.DOLocalMove(targetImage.localPosition - hoverOffset, duration)
-                   .SetEase(Ease.OutBack);
+       // if (!_isDragging) targetImage.DOLocalMove(targetImage.localPosition - hoverOffset, duration)
+                //   .SetEase(Ease.OutBack);
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _isDragging = true; 
+        offset = transform.position - _camera.ScreenToWorldPoint(eventData.position);
+        _defaultParent = transform.parent;
+        transform.SetParent(_defaultParent.parent);
+        _canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPos = _camera.ScreenToWorldPoint(eventData.position);
+        newPos.z = 0;
+        transform.position = newPos + offset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _isDragging = false;
+        transform.SetParent(_defaultParent);
+        _canvasGroup.blocksRaycasts = true;
     }
 }
