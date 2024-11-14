76e9de89460c4f2b4fb5e5bdbd800dd03702adf3
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-11-14 17:25:08 +0300

    save: ability sketch
    Добавил базовый класс способности.

---
+++ Assets/Scripts/Gameplay/Ability/Ability.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Gameplay.Characters;
+
+namespace Assets.Scripts.Gameplay.Ability
+{
+
+    public abstract class Ability
+    {
+        protected CharacterData _playerData;
+        protected CharacterData _enemyData;
+        protected Ability(CharacterData playerData, CharacterData enemyData)
+        {
+            _enemyData = enemyData;
+            _playerData = playerData;
+        }
+        public abstract void Activate();
+        
+    }
+}
---
+++ Assets/Scripts/Gameplay/Ability/HealAbility.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Gameplay.Characters;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Ability
+{
+    public class HealAbility : Ability
+    {
+        public HealAbility(CharacterData playerData, CharacterData enemyData) : base(playerData, enemyData)
+        {
+        }
+
+        public override void Activate()
+        {
+            Debug.Log("Хиииил");
+        }
+    }
+}
